2d46fbacd4df50b8c7b4e281a8c554db0a011157
Alexandra Lignell <alexandra.lignell@elev.ga.ntig.se>
2024-11-14 15:41:41 +0100

    whitecollar

--- näsan4/WhiteCollar.cs
+++ näsan4/WhiteCollar.cs
@@ -1,5 +1,17 @@
 class WhiteCollar : Worker
 {
     public WhiteCollar(string word, int number) : base(word, number){}
-    
+
+    public void DrinkCoffee()
+    {
+        System.Console.WriteLine("*slurping noises*");
+    }
+    public void AttendMeeting()
+    {
+        System.Console.WriteLine("zzzzzzz");
+    }
+    public void DoSpreadsheets()
+    {
+        System.Console.WriteLine("taptaptaptap *keyboard noises*");
+    }
 }
